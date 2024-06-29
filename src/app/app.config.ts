100099e0cfd90e179231852a8a76d438e6cad351
GrantNLarsen <grantnlarsen@gmail.com>
2024-06-29 15:04:32 -0500

    Added firebase function functionality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"grant-n-larsen","appId":"1:722494370585:web:1b49ce50dc3f76d3810b88","storageBucket":"grant-n-larsen.appspot.com","apiKey":"AIzaSyAGMKqUEKb4O3fytrDNeKUEZEIYpfQzFKc","authDomain":"grant-n-larsen.firebaseapp.com","messagingSenderId":"722494370585","measurementId":"G-ZS0YC7JS2G"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), providePerformance(() => getPerformance()), provideStorage(() => getStorage())]
 };
